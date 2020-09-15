cf87a2bd1b5594cf9b85a53d8fcab92adcd49662
Foltányi Kolos <kolos65@gmail.com>
2020-09-15 11:04:40 +0200

    Updates mock data source

--- Data/Data Source/CovidStatsMockDataSource.swift
+++ Data/Data Source/CovidStatsMockDataSource.swift
@@ -32,21 +32,21 @@ class CovidStatsMockDataSource: CovidStatsDataSource {
             "error": false,
             "data": {
                 "covid19Stats": [
-                    {"country":"Netherlands","deaths":0,"city":"","keyId":"Aruba, Netherlands","confirmed":62,"lastUpdate":"2020-04-03 22:46:20","recovered":1,"province":"Aruba"},
-                    {"country":"Netherlands","deaths":1,"city":"","keyId":"Curacao, Netherlands","confirmed":11,"lastUpdate":"2020-04-03 22:46:20","recovered":3,"province":"Curacao"},
-                    {"country":"Netherlands","deaths":2,"city":"","keyId":"Sint Maarten, Netherlands","confirmed":23,"lastUpdate":"2020-04-03 22:46:20","recovered":6,"province":"Sint Maarten"},
-                    {"country":"Belgium","deaths":1143,"city":"","keyId":"Belgium","confirmed":16770,"lastUpdate":"2020-04-03 22:46:20","recovered":2872,"province":""},
-                    {"country":"Germany","deaths":1275,"city":"","keyId":"Germany","confirmed":91159,"lastUpdate":"2020-04-03 22:46:20","recovered":24575,"province":""},
-                    {"country":"Hungary","deaths":26,"city":"","keyId":"Hungary","confirmed":623,"lastUpdate":"2020-04-03 22:46:20","recovered":43,"province":""},
-                    {"country":"Iran","deaths":3294,"city":"","keyId":"Iran","confirmed":53183,"lastUpdate":"2020-04-03 22:46:20","recovered":17935,"province":""},
-                    {"country":"Italy","deaths":14681,"city":"","keyId":"Italy","confirmed":119827,"lastUpdate":"2020-04-03 22:46:20","recovered":19758,"province":""},
-                    {"country":"Korea, South","deaths":174,"city":"","keyId":"Korea, South","confirmed":10062,"lastUpdate":"2020-04-03 22:46:20","recovered":6021,"province":""},
-                    {"country":"Netherlands","deaths":1487,"city":"","keyId":"Netherlands","confirmed":15723,"lastUpdate":"2020-04-03 22:46:20","recovered":250,"province":""},
-                    {"country":"New Zealand","deaths":1,"city":"","keyId":"New Zealand","confirmed":868,"lastUpdate":"2020-04-03 22:46:20","recovered":103,"province":""},
-                    {"country":"Poland","deaths":71,"city":"","keyId":"Poland","confirmed":3383,"lastUpdate":"2020-04-03 22:46:20","recovered":56,"province":""},
-                    {"country":"Russia","deaths":34,"city":"","keyId":"Russia","confirmed":4149,"lastUpdate":"2020-04-03 22:46:20","recovered":281,"province":""},
-                    {"country":"South Africa","deaths":9,"city":"","keyId":"South Africa","confirmed":1505,"lastUpdate":"2020-04-03 22:46:20","recovered":95,"province":""},
-                    {"country":"Spain","deaths":11198,"city":"","keyId":"Spain","confirmed":119199,"lastUpdate":"2020-04-03 22:46:20","recovered":30513,"province":""}
+                    {"country":"Netherlands","deaths":0,"city":"","keyId":"Aruba, Netherlands","confirmed":62,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":1,"province":"Aruba"},
+                    {"country":"Netherlands","deaths":1,"city":"","keyId":"Curacao, Netherlands","confirmed":11,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":3,"province":"Curacao"},
+                    {"country":"Netherlands","deaths":2,"city":"","keyId":"Sint Maarten, Netherlands","confirmed":23,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":6,"province":"Sint Maarten"},
+                    {"country":"Belgium","deaths":1143,"city":"","keyId":"Belgium","confirmed":16770,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":2872,"province":""},
+                    {"country":"Germany","deaths":1275,"city":"","keyId":"Germany","confirmed":91159,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":24575,"province":""},
+                    {"country":"Hungary","deaths":26,"city":"","keyId":"Hungary","confirmed":623,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":43,"province":""},
+                    {"country":"Iran","deaths":3294,"city":"","keyId":"Iran","confirmed":53183,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":17935,"province":""},
+                    {"country":"Italy","deaths":14681,"city":"","keyId":"Italy","confirmed":119827,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":19758,"province":""},
+                    {"country":"Korea, South","deaths":174,"city":"","keyId":"Korea, South","confirmed":10062,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":6021,"province":""},
+                    {"country":"Netherlands","deaths":1487,"city":"","keyId":"Netherlands","confirmed":15723,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":250,"province":""},
+                    {"country":"New Zealand","deaths":1,"city":"","keyId":"New Zealand","confirmed":868,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":103,"province":""},
+                    {"country":"Poland","deaths":71,"city":"","keyId":"Poland","confirmed":3383,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":56,"province":""},
+                    {"country":"Russia","deaths":34,"city":"","keyId":"Russia","confirmed":4149,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":281,"province":""},
+                    {"country":"South Africa","deaths":9,"city":"","keyId":"South Africa","confirmed":1505,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":95,"province":""},
+                    {"country":"Spain","deaths":11198,"city":"","keyId":"Spain","confirmed":119199,"lastUpdate":"2020-09-15T09:00:26+0000","recovered":30513,"province":""}
                 ],
                 "lastChecked":"20-04-04T11:06:19.307+0000"
             }
